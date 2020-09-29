4141baff8833a83d05ed0386b4d58c69c775dc94
Sean Thunquest <sthunquest@gmail.com>
2020-09-29 20:38:45 +0000

    Initial commit of api gateway example

---
+++ svrb/svrb.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.get('/status', function(req, res) {
+    return res.send('Server B\n');
+});
+
+http.createServer(app).listen(9090, function() {
+    console.log('Listening on port 9090');
+});
